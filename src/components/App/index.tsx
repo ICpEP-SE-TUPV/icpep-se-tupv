dfc6b599e6f2680ab7d5e8edf8ca245dc5545acf
Adriane Justine Geneblaza Tan <eidoriantan@gmail.com>
2022-12-09 21:08:41 +0800

    feat: initial changes to template

---
+++ src/components/App/index.tsx
@@ -0,0 +1,13 @@
+
+import React from 'react';
+import './style.css';
+
+class App extends React.Component {
+  render () {
+    return (
+      <div>Hello</div>
+    );
+  }
+}
+
+export default App;
