01f9848806533eb7751612c5cc54d2ff7b771c8f
Adriane Justine Geneblaza Tan <eidoriantan@gmail.com>
2022-12-13 00:49:31 +0800

    feat: add homepage banner

---
+++ src/components/Carousel/index.tsx
@@ -0,0 +1,66 @@
+
+import React from 'react';
+
+import { ReactComponent as LeftIcon } from '../../assets/left.svg';
+import { ReactComponent as RightIcon } from '../../assets/right.svg';
+import './style.scss';
+
+interface CarouselProps {
+  images: string[];
+}
+
+interface CarouselState {
+  current: number;
+}
+
+class Carousel extends React.Component<CarouselProps, CarouselState> {
+  constructor (props: CarouselProps) {
+    super(props);
+
+    this.state = {
+      current: 0
+    };
+
+    this.prev = this.prev.bind(this);
+    this.next = this.next.bind(this);
+  }
+
+  prev (event: React.MouseEvent) {
+    const current = this.state.current;
+    if (current > 0) this.setState({ current: current - 1 });
+  }
+
+  next (event: React.MouseEvent) {
+    const current = this.state.current;
+    const max = this.props.images.length;
+    if (current < max - 1) this.setState({ current: current + 1 });
+  }
+
+  render () {
+    const images = this.props.images;
+    const current = this.state.current;
+    const items: React.ReactNode[] = [];
+
+    for (let i = 0; i < images.length; i++) {
+      const image = images[i];
+      items.push(
+        <div className={`carousel-item ${current === i ? 'active' : ''}`} key={i}>
+          <img src={image} alt={`Carousel Item #${i + 1}`} height={600} />
+        </div>
+      );
+    }
+
+    return (
+      <div className="carousel">
+        <LeftIcon width={24} fill="currentColor" onClick={this.prev} />
+        <div className="carousel-bg" style={{ backgroundImage: `url("${images[current]}")` }}></div>
+        <div className="carousel-items">
+          { items }
+        </div>
+        <RightIcon width={24} fill="currentColor" onClick={this.next} />
+      </div>
+    );
+  }
+}
+
+export default Carousel;
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -4,6 +4,7 @@ import axios from 'axios';
 
 import PageTemplate from '../PageTemplate';
 import Article from '../../components/Article';
+import Carousel from '../../components/Carousel';
 import { IArticle } from '../../types';
 import { ReactComponent as FacebookIcon } from '../../assets/facebook.svg';
 import { ReactComponent as DiscordIcon } from '../../assets/discord.svg';
@@ -46,23 +47,29 @@ class Home extends React.Component<HomeProps, HomeState> {
 
   render () {
     const articles: React.ReactNode[] = [];
+    const banners: string[] = [];
 
     for (let i = 0; i < this.state.articles.length; i++) {
       const article = this.state.articles[i];
       articles.push(<Article article={article} key={i} />);
+      banners.push(article.images[0]);
     }
 
     return (
       <PageTemplate>
         <main>
           <div className="section">
+            <Carousel images={banners} />
+          </div>
+
+          <div className="section pt-4">
             <h4 className="section-title">UPDATES</h4>
             <div className="articles">
               { articles }
             </div>
           </div>
 
-          <div className="section pt-5">
+          <div className="section pt-4">
             <h4 className="section-title">SOCIAL MEDIAS</h4>
             <div className="socials">
               <a href="https://www.facebook.com/ICpEP.seTUPV" target="_blank" rel="noreferrer">
