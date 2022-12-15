03984758bde6c724544e0d7f9b5421cbcbd26266
Adriane Justine Geneblaza Tan <eidoriantan@gmail.com>
2022-12-15 18:26:57 +0800

    perf: enable strict mode

--- src/index.tsx
+++ src/index.tsx
@@ -13,11 +13,11 @@ const root = ReactDOM.createRoot(
 );
 
 root.render(
-  // <React.StrictMode>
+  <React.StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  // </React.StrictMode>
+  </React.StrictMode>
 );
 
 reportWebVitals(console.log);
